b0a477ca39b3ec93f3c8149b66b44ba98b09d12a
AQP20171679 <alejandraadriantejada@gmail.com>
2017-10-06 14:57:54 -0500

    Curso jS completo

--- app.jsx
+++ app.jsx
@@ -156,34 +156,34 @@ class Courses extends React.Component {
         
         this.js = [
             {
-                img: "img/html1.png",
-                course: "How to Make a Website",
-                text: "If you’ve never built a website before and you have no coding or design experience, this is the place to start. In this project, we learn how to build a modern portfolio website for desktops, tablets, and mobile devices. We start with basic HTML and CSS syntax. Next, we learn how to build custom web pages with an image gallery and contact page. Finally, we walk through how to share a website live on the web."
+                img: "img/js1.png",
+                course: "JavaScript Basics",
+                text: "JavaScript is a programming language that drives the web: from front-end user interface design, to backend server-side programming, you'll find JavaScript at every stage of a web site and web application. In this course, you'll learn the fundamental programming concepts and syntax of the JavaScript programming language."
             },
             {
-                img: "img/html2.png",
-                course: "HTML forms",
-                text: "The web is a two-way communication medium. There’s lots of HTML elements for displaying data and producing output, and conversely, there’s also lots of HTML elements for accepting input. Accepting input from the user means creating web forms. In this course, we’ll learn about all the most important form elements that web professionals use on a daily basis."
+                img: "img/js2.png",
+                course: "JavaScript Loops, Arrays and Objects",
+                text: "Storing, tracking and handling data is a large part of computer programming. Arrays provide a method for storing multiple values into a single variable. That makes an array a convenient way to pass around a list of items."
             },
             {
-                img: "img/html3.png",
-                course: "HTML Video and Audio",
-                text: "Text and images have always been the foundation of web content, but more than ever, video and audio are also a part of that content mix. Fortunately, we can now create standards-based video and audio players that don't require the use of plugins. Adding video and audio to a webpage is almost as easy as adding an image or formatting some text."
+                img: "img/js3.png",
+                course: "jQuery Basics",
+                text: "jQuery Basics covers why you'd want to use jQuery, what it is and how to include it in your projects. You'll build several projects over the course to give you the confidence to integrate jQuery in your own projects and add that level of flair and interactivity to any site you work on."
             },
             {
-                img: "img/html4.png",
-                course: "SVG Basics",
-                text: "Scalable Vector Graphics (SVG) is an XML markup language for creating two-dimensional images using vectors. This is different from traditional raster-based image formats that use pixels, like JPEG and PNG. When used on web pages, SVG images provide an infinite level of detail, so they look sharp regardless of screen size or pixel density."
+                img: "img/js4.png",
+                course: "AJAX Basics",
+                text: "AJAX is an important front-end web technology that lets JavaScript communicate with a web server. It lets you load new content without leaving the current page, creating a better, faster experience for your web site's visitors. In this course, you'll learn how AJAX works and how you can use JavaScript to communicate with a web server."
             },
             {
-                img: "img/html5.png",
-                course: "Responsive Imagenes",
-                text: "Using the new source-set and sizes attributes, and the new picture element, it's possible to create images that behave better in a responsive design. These new pieces of markup allow us to deliver the right image to the right device, based on resolution, pixel density, and other factors we define. This will help web pages load faster and look better, and the most capable devices will get the best looking images possible."
+                img: "img/js5.png",
+                course: "Interactive Web Pages with JavaScript",
+                text: "In this course we’ll create a to-do list application using JavaScript alone, without using any third party libraries. You’ll get to grips with manipulating and traversing the DOM and adding event handlers to web page elements. The things that you can do with JavaScript are always increasing and this course will give you a solid footing going forward."
             },
             {
-                img: "img/html6.png",
-                course: "Introduction to HTML and CSS",
-                text: "Get started creating web pages with HTML and CSS, the basic building blocks of web development. HTML, or Hypertext Markup Language, is a standard set of tags you will use to tell the web browser how the content of your web pages and applications are structured. Use CSS, or Cascading Style Sheets, to select HTML tags and tell the browser what your content should look like."
+                img: "img/js6.png",
+                course: "Node.js Basics",
+                text: "In this course we will create a command line application to retrieve user's profile information from the Treehouse website. We'll be writing our application in JavaScript to run on the Node.js platform."
             },
         ]
     }
